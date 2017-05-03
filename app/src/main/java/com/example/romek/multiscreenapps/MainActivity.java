eb1251065b6ccc765ff8555bc94bb39652e3a844
rvanboxtel <rvanboxtel@roc-dev.com>
2017-05-03 09:29:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/romek/multiscreenapps/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.romek.multiscreenapps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //sets main intent
+        setContentView(R.layout.activity_main);
+
+        //Finds the button for sporting
+        TextView sporting = (TextView)findViewById(R.id.firstlocation);
+
+        //Makes onclick listenner
+        sporting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create new intent
+                Intent sportingIntent = new Intent(MainActivity.this, SportingAlmereActivity.class);
+
+                //starts the new activity
+                startActivity(sportingIntent);
+            }
+        });
+    }
+}
+
+
---
+++ app/src/main/java/com/example/romek/multiscreenapps/SportingAlmereActivity.java
@@ -0,0 +1,13 @@
+package com.example.romek.multiscreenapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SportingAlmereActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sporting);
+    }
+}
