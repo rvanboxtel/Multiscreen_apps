568b3fc3323405a59756460dc13b6fa615a157b0
rvanboxtel <rvanboxtel@roc-dev.com>
2017-05-04 08:49:10 +0200

    Update 2: back to main menu button

--- app/src/main/java/com/example/romek/multiscreenapps/SportingAlmereActivity.java
+++ app/src/main/java/com/example/romek/multiscreenapps/SportingAlmereActivity.java
@@ -1,7 +1,10 @@
 package com.example.romek.multiscreenapps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SportingAlmereActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SportingAlmereActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sporting);
+        //Finds the button for sporting
+        final TextView backToMenu = (TextView)findViewById(R.id.back);
+
+        //Makes onclick listenner
+        backToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create new intent
+                Intent backToMain = new Intent(SportingAlmereActivity.this, MainActivity.class);
+
+                //starts the new activity
+                startActivity(backToMain);
+            }
+        });
     }
 }
